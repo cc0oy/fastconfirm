f58289767fcdc0d912fa49bad6ddf3dc1048e0ae
cc0oy <d_hanyue@outlook.com>
2022-02-27 17:07:39 +0800

    txs receive test

--- fastconfirm/core/fastconfirm.py
+++ fastconfirm/core/fastconfirm.py
@@ -431,6 +431,7 @@ class Fastconfirm:
                     atx = self._recv_txs()
                     print("node {} receive transaction {}".format(self.id,atx))
                     self.transaction_buffer.put_nowait(atx)
+                    print("now transaction buffer size {}".format(self.transaction_buffer.qsize()))
                 except Exception as e:
                     # print(str(self.id)+":"+str((e,traceback.print_exc())))
                     # print("node {} receive txs message error".format(self.id))
