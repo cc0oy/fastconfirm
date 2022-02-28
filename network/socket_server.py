0b8dec8721d84ac4a12b72f384c78a4e84c34c44
cc0oy <d_hanyue@outlook.com>
2022-02-28 11:19:19 +0800

    streamserver test

--- network/socket_server.py
+++ network/socket_server.py
@@ -62,7 +62,7 @@ class NetworkServer (Process):
             except Exception as e:
                 self.logger.error(str((e, traceback.print_exc())))
 
-        self.streamServer = StreamServer((self.ip, self.port), _handler)
+        self.streamServer = StreamServer((self.ip, self.port), _handler).serve_forever()
         self.logger.info("invoke streamServer with ({},{}) object {}".format(self.ip,self.port,type(self.streamServer)))
 
         try:
