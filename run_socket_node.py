7f7e9714fa4959ad339ae70842c8f051ca5692f1
cc0oy <d_hanyue@outlook.com>
2022-04-30 20:03:55 +0800

    fix

--- run_socket_node.py
+++ run_socket_node.py
@@ -220,7 +220,7 @@ if __name__ == '__main__':
     with net_ready.get_lock():
         net_ready.value = True
 
-    while time.time() < 1651303431.789273:
+    while time.time() < 1651320350.312475:
         time.sleep(0.0001)
 
     print("see the bft start time {}".format(time.time()))
